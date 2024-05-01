b5c0439ad56d9dfd5faa2fa9917ce65556f20217
Jeancarlo <jcarlo_p_p@hotmail.com>
2024-04-30 22:48:19 -0500

    añadiendo domain y persistance

---
+++ src/main/java/com/project/coches/coches/domain/pojo/MarcaCoche.java
@@ -0,0 +1,15 @@
+package com.project.coches.coches.domain.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/* Pojo de marca coche */
+
+@Setter
+@Getter
+public class MarcaCoche {
+
+    private Integer id;
+    private String description;
+
+}
